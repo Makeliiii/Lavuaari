20d14b90ab5da39ef7f920a1ff3ea6cb72435468
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-30 16:51:35 +0200

    upload not done, start working on link download

--- src/commands/memes/Upload.ts
+++ src/commands/memes/Upload.ts
@@ -12,29 +12,41 @@ export default class UploadCommand extends Command {
             ratelimit: 1,
             args: [
                 {
+                    id: 'link',
+                    match: 'flag',
+                    flag: ['--link', '-l']
+                },
+                {
                     id: 'name',
                     type: 'string',
-                    match: 'content'
+                    match: 'rest'
                 }
             ]
         })
     }
 
-    public async exec(message: Message, { name }: { name: string }) {
-        const attachment = message.attachments.first()
-        const extension = attachment!.name!.split('.').pop()
-        const acceptExt = /\.(webm|mp4)$/i
+    public async exec(message: Message, { link, name }: { link: boolean, name: string }) {
+        if (!link) {
+            const attachment = message.attachments.first()
+            if (!attachment) return message.channel.send('Please provide an attachment!')
 
-        if (!attachment) return message.channel.send('Please provide an attachment!')
-        if (!acceptExt.test(attachment.name!)) return message.channel.send('Only webm and mp4 allowed.')
-        if (!name) name = attachment.name!.split('.')[0]!
+            const acceptExt = /\.(webm|mp4)$/i
 
-        await download(attachment.url, join(__dirname, '..', '..', '..', 'uploads', `${name.split(' ').join('_')}.${extension}`))
-            .then(() => message.channel.send('Upload success!'))
-            .catch(err => {
-                return console.log(err)
-            })
+            if (!acceptExt.test(attachment.name!)) return message.channel.send('Only webm and mp4 allowed.')
+            if (!name) name = attachment.name!.split('.')[0]!
 
-        return console.log(name, message.attachments.first())
+            const extension = attachment!.name!.split('.').pop()
+    
+            await download(attachment.url, join(__dirname, '..', '..', '..', 'uploads', `${name.split(' ').join('_')}.${extension}`))
+                .then(() => message.channel.send('Upload success!'))
+                .catch(err => {
+                    return console.log(err)
+                })
+            console.log(name, attachment)
+        }
+
+        if (!name) return message.channel.send('Please provide a link to the content you want uploaded!')
+
+        return console.log(name)
     }
 }
