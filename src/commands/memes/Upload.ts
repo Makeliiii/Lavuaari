1a1f9c2bc660cdfc2d27e05a97b1c51a34829db4
Mäkeli <joel.m.maenpaa@gmail.com>
2020-11-30 15:36:42 +0200

    upload functionality basically done

--- src/commands/memes/Upload.ts
+++ src/commands/memes/Upload.ts
@@ -1,5 +1,7 @@
 import { Command } from 'discord-akairo'
 import { Message } from 'discord.js'
+import { join } from 'path'
+import { download } from '../../util/download'
 
 export default class UploadCommand extends Command {
     public constructor() {
@@ -20,9 +22,19 @@ export default class UploadCommand extends Command {
 
     public async exec(message: Message, { name }: { name: string }) {
         const attachment = message.attachments.first()
-        if (!attachment) return message.channel.send('Please provide an attachment!')
-        if (attachment.name !== `webm`) return message.channel.send('Only asd allowed.')
+        const extension = attachment!.name!.split('.').pop()
+        const acceptExt = /\.(webm|mp4)$/i
 
-        return console.log(name.split(' ').join('_'), message.attachments.first())
+        if (!attachment) return message.channel.send('Please provide an attachment!')
+        if (!acceptExt.test(attachment.name!)) return message.channel.send('Only webm and mp4 allowed.')
+        if (!name) name = attachment.name!.split('.')[0]!
+
+        await download(attachment.url, join(__dirname, '..', '..', '..', 'uploads', `${name.split(' ').join('_')}.${extension}`))
+            .then(() => message.channel.send('Upload success!'))
+            .catch(err => {
+                return console.log(err)
+            })
+
+        return console.log(name, message.attachments.first())
     }
 }
