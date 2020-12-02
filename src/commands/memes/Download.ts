147551ac761a8b92754aa4ffd3b4b523136c3adc
Mäkeli <joel.m.maenpaa@gmail.com>
2020-12-02 17:49:09 +0200

    comments pog

--- src/commands/memes/Download.ts
+++ src/commands/memes/Download.ts
@@ -26,17 +26,23 @@ export default class DownloadCommand extends Command {
     }
 
     public async exec(message: Message, { link, name }: { link: string, name: string }) {
+        // accept only these extensions... perhaps more in the future
         const acceptExt = /\.(webm|mp4)$/i
 
+        // if link argument is falsy; not provided
         if (!link) {
             const attachment = message.attachments.first()
 
+            // check for some input errors and mutate name
+            // dunno if mutating name is a good idea lul
             if (!attachment) return message.channel.send('Please provide an attachment!')
             if (!acceptExt.test(attachment.name!)) return message.channel.send('Only webm and mp4 allowed.')
             if (!name) name = attachment.name!.split('.')[0]!
 
+            // get the file extension
             const extension = attachment!.name!.split('.').pop()
     
+            // download file from the attachment url
             await download(attachment.url, join(__dirname, '..', '..', '..', 'downloads', `${name.split(' ').join('_')}.${extension}`))
                 .then(() => message.channel.send('Download success!'))
                 .catch(err => {
@@ -45,14 +51,17 @@ export default class DownloadCommand extends Command {
                 })
         }
 
+        // again check for some input errors and mutate name
         if (!isURL(link)) return message.channel.send('Flag --link was used...\nSo provide a link mate!')
         if (!acceptExt.test(link)) return message.channel.send('Only webm and mp4 allowed.')
         if (!name) return message.channel.send('Please give a name for links mate!')
 
+        // get the file extension
         const extension = link.split('.').filter(string => string === 'mp4' || string === 'webm')
 
         console.log(`Link: ${link},\nName: ${name}\nTest: ${acceptExt.test(link)},\nFile Extension: ${extension}`)
 
+        // download file from link
         await download(link, join(__dirname, '..', '..', '..', 'downloads', `${name}.${extension}`))
             .then(() => message.channel.send('Download success!'))
             .catch(err => {
