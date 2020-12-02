2ee6ad9eff4d203801858e6ec9199b6fa497120e
Mäkeli <joel.m.maenpaa@gmail.com>
2020-12-02 23:48:28 +0200

    combined list and search command cause it makes more sense

--- src/commands/Search.ts
+++ src/commands/Search.ts
@@ -7,10 +7,15 @@ export default class SearchCommand extends Command {
     public constructor() {
         super('search', {
             aliases: ['search', 's'],
-            description: 'Search files by filter.',
+            description: 'Search files by filter or provide the flag --all to list all files.',
             category: 'Memes',
             ratelimit: 1,
             args: [
+                {
+                    id: 'all',
+                    match: 'flag',
+                    flag: ['--all', '-a']
+                },
                 {
                     id: 'filter',
                     match: 'content'
@@ -19,23 +24,42 @@ export default class SearchCommand extends Command {
         })
     }
 
-    public exec(message: Message, { filter }: { filter: string }) {
+    public exec(message: Message, { all, filter }: { all: boolean, filter: string }) {
         const folder = join(__dirname, '..', '..', 'downloads')
-        const files = fs.readdirSync(folder).filter(file => file.includes(filter))
 
-        if (!filter) return message.channel.send('Please include a search parameter.')
-        if (!files.length) return message.channel.send('No files found!')
+        if (!all) {
+            const files = fs.readdirSync(folder).filter(file => file.includes(filter))
 
-        const embed = new MessageEmbed()
-            .setTitle('File(s)')
-            .addField(
-                '\u200B',
-                files.map(file => {
-                    const noExtFile = file.split('.')[0]
-                    return noExtFile
-                })
-            )
+            if (!filter) return message.channel.send('Please include a search parameter.')
+            if (!files.length) return message.channel.send('No files found!')
+    
+            const embed = new MessageEmbed()
+                .setTitle('File(s)')
+                .addField(
+                    '\u200B',
+                    files.map(file => {
+                        const noExtFile = file.split('.')[0]
+                        return noExtFile
+                    })
+                )
+    
+            return message.channel.send(embed)
+        } else {
+            const files = fs.readdirSync(folder)
 
-        return message.channel.send(embed)
+            if (!files.length) return message.channel.send('No files found!')
+    
+            const embed = new MessageEmbed()
+                .setTitle('File(s)')
+                .addField(
+                    '\u200B',
+                    files.map(file => {
+                        const noExtFile = file.split('.')[0]
+                        return noExtFile
+                    })
+                )
+    
+            return message.channel.send(embed)
+        }
     }
 }
