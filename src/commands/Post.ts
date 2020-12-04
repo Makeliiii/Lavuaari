75c91b88f7e5fceec2a0d6e259a81afccdfdaf3f
Mäkeli <joel.m.maenpaa@gmail.com>
2020-12-04 13:27:06 +0200

    post command done

--- src/commands/Post.ts
+++ src/commands/Post.ts
@@ -19,20 +19,44 @@ export default class PostCommand extends Command {
         })
     }
 
-    public exec(message: Message, { name }: { name: string }) {
+    public async exec(message: Message, { name }: { name: string }) {
         if (!name) return message.channel.send('How about you provide a parameter so I can search for the file?!?!')
 
         const folder = join(__dirname, '..', '..', 'downloads')
         const filter = name.split(' ').join('_')
-        const file = fs.readdirSync(folder).filter(file => file.includes(filter))
+        const files = fs.readdirSync(folder).filter(file => file.includes(filter))
         let filePath
 
-        if (!file.length) return message.channel.send('No files found!')
-        if (file.length === 1) {
-            filePath = join(__dirname, '..', '..', 'downloads', file[0])
-            return message.channel.send(`Found a file with parameter ${name}:`, { files: [filePath] })
-        }
+        if (!files.length) return message.channel.send('No files found!')
+        if (files.length > 1) {
+            const fileMap = files.map((file, index) => `**${index + 1}.** ${file.split('_').join(' ')}\n`).join('')
+            let fileNumbers: number[] = []
 
-        return message.channel.send(`Searched with param ${name}`)
+            for (const file of files) {
+                const num = files.indexOf(file)
+                fileNumbers.push(num + 1)
+            }
+
+            const filter = (response: any) => {
+                return fileNumbers.some(number => number.toString() === response.content.toString())
+            }
+
+            message.channel.send(`Found these files with given parameters:\n\n${fileMap}\n**Respond back with the corresponding number plox**`).then(async () => {
+                    await message.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
+                        .then(collected => {
+                            const index = parseInt(collected.first()!.content) - 1
+                            const file = files[index]
+                            console.log(files[index])
+                            filePath = join(__dirname, '..', '..', 'downloads', file)
+                            message.channel.send(`Posting ${file}`, { files: [filePath] })
+                        })
+                        .catch(collected => {
+                            message.channel.send('Yea ok nevermind then...')
+                        })
+                })
+        } else {
+            filePath = join(__dirname, '..', '..', 'downloads', files[0])
+            return message.channel.send(`Found ${name}:`, { files: [filePath] })
+        }
     }
 }
